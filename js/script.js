8c7a8881e79c7c98a8a33f2f26676715b5870987
Rondell Borel <Rondell@localhost.localdomain>
2020-09-29 17:52:04 -0400

    added ability to stored theme preference

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 const toggle = document.getElementById('toggle');
 const toggleSwitch = document.getElementById('toggleSwitch');
+const theme = localStorage.getItem('theme');
 
 toggleSwitch.addEventListener('click', toggleTheme);
+
+if (theme === 'dark') {
+    toggle.classList.toggle('toggle-dark');
+    document.documentElement.setAttribute('data-theme', theme);
+}
 
 function toggleTheme(ev) {
 
     if (toggle.classList.toggle('toggle-dark')) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.removeAttribute('data-theme');
+        localStorage.removeItem('theme');
     }
 }
